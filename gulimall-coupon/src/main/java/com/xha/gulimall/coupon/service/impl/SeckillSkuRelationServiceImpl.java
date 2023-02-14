02f6697b488fa8f10052d71f982f904ac350a82f
“XuHuaiAng” <2533694604@qq.com>
2023-02-14 11:00:06 +0800

    添加秒杀模块

--- gulimall-coupon/src/main/java/com/xha/gulimall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
+++ gulimall-coupon/src/main/java/com/xha/gulimall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
@@ -1,16 +1,16 @@
 package com.xha.gulimall.coupon.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xha.gulimall.common.utils.PageUtils;
 import com.xha.gulimall.common.utils.Query;
-
 import com.xha.gulimall.coupon.dao.SeckillSkuRelationDao;
 import com.xha.gulimall.coupon.entity.SeckillSkuRelationEntity;
 import com.xha.gulimall.coupon.service.SeckillSkuRelationService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("seckillSkuRelationService")
@@ -18,9 +18,11 @@ public class SeckillSkuRelationServiceImpl extends ServiceImpl<SeckillSkuRelatio
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        LambdaQueryWrapper<SeckillSkuRelationEntity> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(SeckillSkuRelationEntity::getPromotionSessionId, params.get("promotionSessionId"));
         IPage<SeckillSkuRelationEntity> page = this.page(
                 new Query<SeckillSkuRelationEntity>().getPage(params),
-                new QueryWrapper<SeckillSkuRelationEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
